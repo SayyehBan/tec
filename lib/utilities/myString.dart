dadaa1b8329e6d2b6bc9c9b93c7b8b732cdcba30
SayyehBan <sdvp1991david@gmail.com>
2023-04-29 23:25:57 +0330

    افزودن package و متغییر های اول

---
+++ lib/utilities/myString.dart
@@ -0,0 +1,7 @@
+class myStrings {
+  static const String viewHottestPosts = "مشاهده داغ ترین نوشته ها";
+  static const String viewHottestPodcasts = "مشاهده داغ ترین پادکست ها";
+  static const String relatedPosts = "نوشته های مرتبط";
+  static const String submitArticle = "ارسال مقاله";
+  static const String submitPodcast = "ارسال پادکست";
+}
