c9e468296d23ab8d0fb955c2be18b6aec47ad83b
SayyehBan <sdvp1991david@gmail.com>
2023-05-02 11:58:20 +0330

    PodcastsBlog

--- lib/screen/home/homeScreen.dart
+++ lib/screen/home/homeScreen.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tec/gen/assets.gen.dart';
 import 'package:tec/screen/home/blogList.dart';
+import 'package:tec/screen/home/podcastsBlog.dart';
 import 'package:tec/screen/home/poster.dart';
 import 'package:tec/screen/home/tagList.dart';
 import 'package:tec/screen/home/titleBlog.dart';
@@ -16,40 +18,50 @@ class HomScreen extends StatelessWidget {
     double bodyMargin = size.width / 10;
     return SafeArea(
       child: Scaffold(
-          body: Padding(
-        padding: const EdgeInsets.only(top: 20),
-        child: Column(
-          children: [
-            //appBar
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                const Icon(Icons.menu),
-                Assets.images.logo.image(height: size.height / 13.6),
-                const Icon(Icons.search),
-              ],
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            const Poster(),
-            const SizedBox(
-              height: 16,
-            ),
+          body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: Column(
+            children: [
+              //appBar
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Icon(Icons.menu),
+                  Assets.images.logo.image(height: size.height / 13.6),
+                  const Icon(Icons.search),
+                ],
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const Poster(),
+              const SizedBox(
+                height: 16,
+              ),
 
-            ///tagList
-            const TagList(),
-            const SizedBox(
-              height: 32,
-            ),
-            //See More
-            TitleBlog(
-              image: Assets.icons.bluePen.path,
-              title: myStrings.viewHottestPosts,
-            ),
-            //Blog List
-            const BlogList()
-          ],
+              ///tagList
+              const TagList(),
+              const SizedBox(
+                height: 32,
+              ),
+              //See More
+              TitleBlog(
+                image: Assets.icons.bluePen.path,
+                title: myStrings.viewHottestPosts,
+              ),
+              //Blog List
+              const BlogList(),
+              //See More
+              TitleBlog(
+                image: Assets.icons.blueMic.path,
+                title: myStrings.viewHottestPodcasts,
+              ),
+              //Podcasts Blog
+              const PodcastsBlog()
+            ],
+          ),
         ),
       )),
     );
