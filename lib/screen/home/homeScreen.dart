d331f8f8fcb86c9e7ee278b4fed52987b97d2a7b
SayyehBan <sdvp1991david@gmail.com>
2023-05-04 21:58:36 +0330

    Update homeScreen.dart

--- lib/screen/home/homeScreen.dart
+++ lib/screen/home/homeScreen.dart
@@ -3,18 +3,27 @@ import 'package:tec/screen/home/appBarHome.dart';
 import 'package:tec/screen/home/navBarHome.dart';
 import 'package:tec/screen/profiler/profileScreen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedPageIndex = 0;
+  @override
   Widget build(BuildContext context) {
+    List<Widget> tectMainScreenPages = const [HomeScreen(), ProfileScreen()];
     return SafeArea(
       child: Scaffold(
         appBar: const AppBarHome(),
         body: Stack(
-          children: const [
-            Center(child: Positioned.fill(child: ProfileScreen())),
-            NavBarHome(),
+          children: [
+            Center(
+                child: Positioned.fill(
+                    child: tectMainScreenPages[selectedPageIndex])),
+            const NavBarHome(),
           ],
         ),
       ),
