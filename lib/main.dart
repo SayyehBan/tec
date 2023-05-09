eb543b1529d8ff454ba55cedfc2d6e8f95430a3a
SayyehBan <sdvp1991david@gmail.com>
2023-05-10 00:35:29 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tec/gen/fonts.gen.dart';
+import 'package:tec/screen/splash/splashScreen.dart';
 import 'package:tec/utilities/myColors.dart';
-
-import 'screen/register/registerIntro.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
@@ -103,7 +102,7 @@ class MyApp extends StatelessWidget {
       supportedLocales: const [
         Locale('fa'), // Persian
       ],
-      home: const RegisterIntro(),
+      home: const SplashScreen(),
     );
   }
 }
