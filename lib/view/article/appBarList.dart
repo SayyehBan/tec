d16c944c9e00bc4913b05dad388aa3290ee91553
SayyehBan <sdvp1991david@gmail.com>
2023-05-13 15:54:52 +0330

    لیست مقالات

--- lib/view/article/appBarList.dart
+++ lib/view/article/appBarList.dart
@@ -4,45 +4,37 @@ import 'package:flutter/material.dart';
 import 'package:tec/utilities/myColors.dart';
 import 'package:tec/utilities/textStyle.dart';
 
-class AppBarList extends StatelessWidget {
-  const AppBarList({
-    super.key,
-    required this.title,
-  });
-  final String title;
-  @override
-  Widget build(BuildContext context) {
-    return PreferredSize(
-      preferredSize: const Size.fromHeight(80),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(left: 16),
-              child: Center(
-                child: Text(
-                  title,
-                  style: appBarTextStyle,
-                ),
+PreferredSize appBarList(String title) {
+  return PreferredSize(
+    preferredSize: const Size.fromHeight(60),
+    child: Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Center(
+              child: Text(
+                title,
+                style: appBarTextStyle,
               ),
-            )
-          ],
-          leading: Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Container(
-              height: 40,
-              width: 40,
-              decoration: BoxDecoration(
-                  color: SolidColors.primaryColor.withBlue(100),
-                  shape: BoxShape.circle),
-              child: const Icon(Icons.keyboard_arrow_right_rounded),
             ),
+          )
+        ],
+        leading: Padding(
+          padding: const EdgeInsets.only(right: 16),
+          child: Container(
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+                color: SolidColors.primaryColor.withBlue(100),
+                shape: BoxShape.circle),
+            child: const Icon(Icons.keyboard_arrow_right_rounded),
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
--- lib/view/article/articleListScreen.dart
+++ lib/view/article/articleListScreen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tec/controller/article_Controller.dart';
 import 'package:tec/utilities/loading.dart';
+import 'package:tec/utilities/useCachedNetworkImage.dart';
 import 'package:tec/view/article/appBarList.dart';
 
 class ArticleListScreen extends StatelessWidget {
@@ -11,28 +12,77 @@ class ArticleListScreen extends StatelessWidget {
   ArtcileController artcileController = Get.put(ArtcileController());
   @override
   Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
     return SafeArea(
-        child: Scaffold(
-      body: Obx(
-        () => artcileController.loading.value == true
-            ? const Loading()
-            : Column(
-                children: [
-                  const AppBarList(title: "مقالات جدید"),
-                  SizedBox(
-                    height: 100,
-                    child: ListView.builder(
+      child: Scaffold(
+        appBar: appBarList("مقالات جدید"),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            child: Obx(
+              () => artcileController.loading.value == true
+                  ? const Loading()
+                  : ListView.builder(
                       itemCount: artcileController.articleList.length,
                       scrollDirection: Axis.vertical,
+                      physics: const BouncingScrollPhysics(),
                       itemBuilder: (context, index) {
-                        return Text(
-                            artcileController.articleList[index].title!);
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: Get.width / 3,
+                              height: Get.height / 6,
+                              child: UseCachedNetworkImage(
+                                  url: artcileController
+                                      .articleList[index].image!),
+                            ),
+                            const SizedBox(
+                              width: 16,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  width: Get.size.width / 2,
+                                  child: Text(
+                                    artcileController.articleList[index].title!,
+                                    style: themeData.textTheme.headlineMedium,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 16,
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      artcileController
+                                          .articleList[index].author!,
+                                      style: themeData.textTheme.headlineMedium,
+                                    ),
+                                    const SizedBox(
+                                      width: 20,
+                                    ),
+                                    Text(
+                                      '${artcileController.articleList[index].view!} بازدید',
+                                      style: themeData.textTheme.headlineMedium,
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        );
                       },
                     ),
-                  )
-                ],
-              ),
+            ),
+          ),
+        ),
       ),
-    ));
+    );
   }
 }
